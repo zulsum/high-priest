fc1db5e6cdf0e5079902fb437d989843494dd683
Yassen Bantchev <yassenb@gmail.com>
2012-12-01 13:51:54 +0200

    Clean Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HighPriest::Application.config.secret_token = '6d4f871b787219dbf204477972eed2ac580705313383c1e7514f79d55faca614074d7b039e0c31d409c334e00d2353e00afc21e92146532fc45681c1cd2fda58'
